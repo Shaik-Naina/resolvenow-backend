43c84de84170728a15b4a1991a2751d40ea881d3
Shaik Naina <shaiknaina40@gmail.com>
2025-06-27 21:08:57 +0530

    Removed all files from GitHub

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, verifyEmail } = require('../controllers/authController');
-
-router.post('/register', registerUser);
-router.get('/verify/:token', verifyEmail);
-
-module.exports = router;
